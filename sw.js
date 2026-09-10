9dd0df9e0de203d519b7bf24f8543777b059de9c
rustyfields <rustyfields@icloud.com>
2026-09-09 23:40:55 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE='oilfield-ops-v13';
+const CORE=['/manifest.webmanifest'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req,{cache:'no-store'}).then(resp=>resp).catch(()=>caches.match('/index.html')));
+    return;
+  }
+  event.respondWith(fetch(req).then(resp=>{
+    const copy=resp.clone();
+    caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{});
+    return resp;
+  }).catch(()=>caches.match(req)));
+});
